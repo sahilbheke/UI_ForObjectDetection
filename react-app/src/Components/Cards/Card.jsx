d69938c6e449db9fe6f7ea2c5d1899138af9a6f4
sahilbheke <sahilbheke3@gmail.com>
2024-04-22 01:49:23 +0530

    final commit 22-4-24 made some changes in card. Cards are good to go

--- react-app/src/Components/Cards/Card.jsx
+++ react-app/src/Components/Cards/Card.jsx
@@ -15,7 +15,8 @@ function Card() {
                 <img src={tree} alt="Card Image" className='card-img'/>
                 <h1 className='card-title'>Tree Detection</h1>
                 <p className='card-discription'>
-                   Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis quo, ut expedita soluta excepturi saepe aspernatur quidem reiciendis dicta, ad, totam velit cumque quod aliquam iure ducimus deserunt sint? Sapiente.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam doloremque repudiandae nisi adipisci pariatur blanditiis qui eos fugiat facere officiis nihil sequi veniam, quasi velit, ipsam, similique nobis! Ipsa, eveniet.
+                   Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis quo, ut expedita soluta excepturi saepe aspernatur quidem reiciendis dicta, ad, totam velit cumque quod aliquam iure ducimus deserunt sint?
                 </p>
                 <a href='tree_detection' className='card-btn'>Detect Now</a>
             </div>
@@ -24,9 +25,10 @@ function Card() {
                 <img src={building} alt="Card Image" className='card-img'/>
                 <h1 className='card-title'>Building Detection</h1>
                 <p className='card-discription'>
-                   Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis quo, ut expedita soluta excepturi saepe aspernatur quidem reiciendis dicta, ad, totam velit cumque quod aliquam iure ducimus deserunt sint? Sapiente.
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis, delectus rem fugit nam atque rerum animi doloremque cupiditate beatae et? Dolores assumenda tempora nostrum repudiandae ea, neque saepe unde sed.
+                   Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis quo, ut expedita soluta excepturi saepe aspernatur quidem reiciendis dicta, ad, totam velit cumque quod aliquam iure ducimus deserunt sint?
                 </p>
-                <a href='tree_detection' className='card-btn'>Learn More</a>
+                <a href='tree_detection' className='card-btn'>Detect Now</a>
             </div>
             
 
