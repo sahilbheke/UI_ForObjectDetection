971e41bf6d37c2536e4aeee0299c6ea977faf6c8
sahilbheke <sahilbheke3@gmail.com>
2024-04-22 00:55:15 +0530

    cleaning navbar

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -4,19 +4,12 @@ import Navbar from "./Components/Navbar/Navbar";
 import Card from "./Components/Cards/Card";
 
 function App() {
-  const [theme, setTheme] = useState('light');
+  
   return (
-
+    
     <div>
-
-      <div className={`container ${theme}`}>
-        <Navbar theme={theme} setTheme={setTheme} />
-        
-        <Card />
-        
-      </div>
-
-      
+        <Navbar/>
+        <Card />     
     </div>
 
   )
--- react-app/src/Components/Navbar/Navbar.jsx
+++ react-app/src/Components/Navbar/Navbar.jsx
@@ -3,21 +3,16 @@ import './Navbar.css'
 import dark from '../../assets/dark3.png'
 import light from '../../assets/light2.png'
 
-const Navbar = ({ theme, setTheme }) =>{
-
-    const toggle_mode = () => {
-        theme == 'light' ? setTheme('dark'): setTheme('light')
-    }
+function Navbar() {
+    
     return (
         <div className="navbar">
             <p>Detect ME</p>
             <ul>
-               
                 <li>Home</li>
                 <li>About</li>
             </ul>
 
-            <img  onClick={()=>{toggle_mode()}} src={theme == 'light' ? light : dark} alt="lk" className="toggle-icon"></img>
         </div>
     )
 }
