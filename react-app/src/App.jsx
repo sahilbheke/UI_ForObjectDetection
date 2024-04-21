e0b3638da6bafe93cdd906b573e3faa9cb7f4f96
sahilbheke <sahilbheke3@gmail.com>
2024-04-22 00:21:46 +0530

    basic card component done

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,20 +1,19 @@
 import React, { useState } from 'react'
 import "./App.css"
 import Navbar from "./Components/Navbar/Navbar";
-// import Card from "./Components/Cards/Card";
+import Card from "./Components/Cards/Card";
 
 function App() {
   const [theme, setTheme] = useState('light');
   return (
 
     <div>
+
       <div className={`container ${theme}`}>
         <Navbar theme={theme} setTheme={setTheme} />
-
-        {/* <div className="App">
-          <Card/>
-        </div> */}
-
+        
+        <Card />
+        
       </div>
 
       
--- react-app/src/Components/Cards/Card.jsx
+++ react-app/src/Components/Cards/Card.jsx
@@ -1,7 +1,36 @@
 import React from 'react'
-
-export const Card = () => {
+import tree from '../../assets/tree.avif'
+import './Card.css'
+// export const Card = () => {
+//     return (
+//         <div>Card</div>
+//     )
+// }
+function Card() {
     return (
-        <div>Card</div>
+        <div className='cards'>
+            <div className='card-container'>
+                <img src={tree} alt="Card Image" className='card-img'/>
+                <h1 className='card-title'>Tree Detection</h1>
+                <p className='card-discription'>
+                   Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis quo, ut expedita soluta excepturi saepe aspernatur quidem reiciendis dicta, ad, totam velit cumque quod aliquam iure ducimus deserunt sint? Sapiente.
+                </p>
+                <a href='tree_detection' className='card-btn'>Detect Now</a>
+            </div>
+
+            {/* <div className='card-container'>
+                <img src={tree} alt="Card Image" className='card-img'/>
+                <h1 className='card-title'>Card Title</h1>
+                <p className='card-discription'>
+                   Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis quo, ut expedita soluta excepturi saepe aspernatur quidem reiciendis dicta, ad, totam velit cumque quod aliquam iure ducimus deserunt sint? Sapiente.
+                </p>
+                <a href='tree_detection' className='card-btn'>Learn More</a>
+            </div> */}
+            
+
+        </div>
+        
     )
 }
+
+export default Card
